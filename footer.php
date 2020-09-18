cbbd7869b912daa0284612983c7c5057e16b7d4d
K-Kyler <khaiquang690@gmail.com>
2020-09-19 00:06:51 +0700

    Add footer component and images

---
+++ footer.php
@@ -0,0 +1,72 @@
+<style>
+    <?php
+        require_once('./CSS/footer.css');
+    ?>
+</style>
+
+<div class="footer">
+    <div class="footer-content">
+        <div class="row">
+            <div class="footer-item logo col-4">
+                <img src="./Images/code.png" class="logo-team">
+                <span>KHV Team</span>
+            </div>
+
+            <div class="footer-item col-4">
+                <h6 class="item-title">Dịch vụ</h6>
+                <p>Phát triển ứng dụng di động</p>
+                <p>Phát triển ứng dụng web</p>
+                <p>Bảo mật mạng</p>
+            </div>
+
+            <div class="footer-item col-4">
+                <h6 class="item-title">Liên hệ</h6>
+                <p>093-802-8109</p>
+                <p>khvteam@gmail.com</p>
+                <p>19 Nguyễn Hữu Thọ, quận 7, TPHCM</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-content-2">
+        <div class="footer-line"></div>
+
+        <div class="footer-license">
+            <div class="footer-socialnetworks">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-google-plus-g"></i>
+                        </a>
+                    </li>
+
+                        <li>
+                        <a href="#">
+                            <i class="fas fa-wifi"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="footer-copyright">© 2020. Bản quyền thuộc về KHV Team.</div>
+        </div>
+    </div>
+</div>
---
+++ layout.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Đặt sân bóng mini trực tiếp và nhanh chóng | Vui chơi thỏa thích</title>
+        <link rel="icon" type="image/png" sizes="16x16" href="./Images/favicon-16x16.png">
+        <link rel="stylesheet" href="./CSS/layout.css?v=<?php echo time(); ?>">
+    </head>
+
+    <body>
+        <!-- Header -->
+        <?php
+            require_once('header.php');
+        ?>
+
+        
+
+        <!-- Footer -->
+        <?php
+            require_once('footer.php');
+		?>
+		
+		<!-- Font Awesome -->
+		<script src="https://kit.fontawesome.com/a65ad4e019.js" crossorigin="anonymous"></script>
+    </body>
+</html>
