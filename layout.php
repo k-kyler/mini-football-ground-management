85829fa822d17608dc17d4d486f4bbff38a3ac1d
K-Kyler <khaiquang690@gmail.com>
2020-09-19 19:32:06 +0700

    Modified layout

--- layout.php
+++ layout.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>KHV | Đặt sân bóng mini trực tiếp, nhanh chóng, vui chơi thỏa thích</title>
+        <title>KHV | Đặt sân bóng mini trực tuyến, nhanh chóng, vui chơi thỏa thích</title>
         <link rel="icon" type="image/png" sizes="16x16" href="./Images/favicon-16x16.png">
         <link rel="stylesheet" href="./CSS/layout.css?v=<?php echo time(); ?>">
     </head>
