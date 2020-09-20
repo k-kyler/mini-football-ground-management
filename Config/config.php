36fc8638b58284921f2454c3159a6ddde78087a9
K-Kyler <khaiquang690@gmail.com>
2020-09-20 14:27:15 +0700

    Add define DB

--- Config/config.php
+++ Config/config.php
@@ -7,6 +7,7 @@
     define('SERVER', '127.0.0.1');
     define('USER', 'kkyler');
     define('PASS', '123456');
+    define('DB', 'mini_football_ground_management');
     define('ROOT', dirname(__FILE__));
     define('HOST', 'http://localhost:81');
 ?>
