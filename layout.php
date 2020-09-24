9edbf232ba9eb7caa93fdba25406533fa12dea7d
K-Kyler <khaiquang690@gmail.com>
2020-09-24 12:13:42 +0700

    Config layout.php

--- layout.php
+++ layout.php
@@ -5,14 +5,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dịch vụ thuê sân bóng mini KHV | Đặt sân bóng mini trực tuyến, nhanh chóng, vui chơi thỏa thích</title>
         <link rel="icon" type="image/png" sizes="16x16" href="./Images/favicon-16x16.png">
+        
         <link rel="stylesheet" href="./CSS/layout.css?v=<?php echo time(); ?>">
     </head>
 
     <body>
-		  	<!-- Font Awesome -->
-		  	<script src="https://kit.fontawesome.com/a65ad4e019.js" crossorigin="anonymous"></script>
 
-            <!-- jQuery -->
-            <script src="./JS/jquery-3.5.1.js"></script>
+
+        <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/a65ad4e019.js" crossorigin="anonymous"></script>
+
+        <!-- jQuery -->
+        <script src="./JS/jquery-3.5.1.js?v=<?php echo time(); ?>"></script>
     </body>
 </html>
