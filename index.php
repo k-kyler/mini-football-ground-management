b685df0933408608c2d46546298958b542c5a648
K-Kyler <khaiquang690@gmail.com>
2020-10-08 21:46:09 +0700

    Add index page with slider main

--- index.php
+++ index.php
@@ -1,12 +1,83 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title></title>
+<style>
+    <?php 
+        require_once('./CSS/slider-main.css');
+    ?>
+</style>
 
-    </head>
-    <body>
-        
-    </body>
-</html>
+<?php
+    // Layout
+    require_once('layout.php');
+
+    // Config
+    require_once('./Config/config.php');
+
+    ?>
+        <div class="wrapper">
+            <!-- Header -->
+            <?php
+                require_once('header.php'); 
+            ?>
+
+            <!-- Index container -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-2">
+
+                    </div>
+
+                    <!-- Content -->
+                    <div class="col-8">
+                        <!-- Slider main -->
+                        <div class="slider-container">
+                            <div class="slider-main" id="sliderMain">
+                                <!-- Slideshow images -->
+                                <?php        
+                                    $db = getDatabase();
+                                    $res = getImages($db);
+
+                                    if ($res != null && $res -> num_rows > 0) {
+                                        while ($data = $res -> fetch_assoc()) {
+                                            $imageSrc = $data['image_src'];
+                                            $imageType = $data['image_type'];
+
+                                            if ($imageType == "slide") {
+                                                ?>
+                                                    <div class="slide fade">
+                                                        <img src="<?= $imageSrc ?>">
+                                                    </div>
+                                                <?php
+                                            }
+                                        }
+                                    }
+                                ?>
+
+                                <!-- Slider navigation -->
+                                <div class="slider-nav">
+                                    <span class="nav-dot"></span>   
+                                    <span class="nav-dot"></span>   
+                                    <span class="nav-dot"></span>   
+                                </div>
+                            </div>
+                        </div>
+                        <!-- ----- -->
+
+                        
+                    </div>
+
+                    <div class="col-2">
+
+                    </div>
+                </div>
+            </div>
+		</div>
+    <?php
+
+    // Footer
+    require_once('footer.php');
+?>
+
+<script>
+    <?php 
+        require_once('./JS/slider-main.js'); 
+    ?>
+</script>
