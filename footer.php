4db816b6f8ed0805f223bc5c9d9fab881b431c73
K-Kyler <khaiquang690@gmail.com>
2020-10-08 22:31:45 +0700

    Change footer content

--- footer.php
+++ footer.php
@@ -8,21 +8,21 @@
     <div class="footer-content">
         <div class="row">
             <div class="footer-item logo col-4">
-                <img src="./Images/code.png" class="logo-team">
-                <span>KHV Dev</span>
+                <img src="./Images/football.svg" class="logo-team">
+                <span>KHV Ground</span>
             </div>
 
             <div class="footer-item col-4">
                 <h6 class="item-title">Dịch vụ</h6>
-                <p>Phát triển ứng dụng di động</p>
-                <p>Phát triển ứng dụng web</p>
-                <p>Bảo mật mạng</p>
+                <p>Sân bóng mini chất lượng</p>
+                <p>Phục vụ nước miễn phí khi đặt sân</p>
+                <p>Đặt sân trực tuyến</p>
             </div>
 
             <div class="footer-item col-4">
                 <h6 class="item-title">Liên hệ</h6>
                 <p>093-802-8109</p>
-                <p>khvteam@gmail.com</p>
+                <p>khvground@gmail.com</p>
                 <p>19 Nguyễn Hữu Thọ, quận 7, TPHCM</p>
             </div>
         </div>
@@ -66,7 +66,7 @@
                 </ul>
             </div>
 
-            <div class="footer-copyright">© 2020. Bản quyền thuộc về KHV Team.</div>
+            <div class="footer-copyright">© 2020. Bản quyền thuộc về KHV Ground.</div>
         </div>
     </div>
 </div>
