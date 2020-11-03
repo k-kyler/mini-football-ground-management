a151d7c9bde593b528f68b24450062db16fbc6c0
K-Kyler <khaiquang690@gmail.com>
2020-11-03 22:07:49 +0700

    Add jQuery select2

--- layout.php
+++ layout.php
@@ -6,22 +6,30 @@
         <title>Dịch vụ thuê sân bóng mini KHV | Đặt sân bóng mini trực tuyến, nhanh chóng, vui chơi thỏa thích</title>
         <link rel="icon" type="image/png" sizes="16x16" href="./Images/favicon-16x16.png">
         
-        <!-- Layout style -->
+        <!-- Layout styles -->
         <link rel="stylesheet" href="./CSS/layout.css?v=<?php echo time(); ?>">
 
         <!-- Font Awesome -->
-        <link rel="stylesheet" href="./Libraries/FontAwesome/css/all.css">
+        <link rel="stylesheet" href="./Libraries/FontAwesome/css/all.css?v=<?php echo time(); ?>">
 
-        <!-- jQuery UI style -->
-        <link rel="stylesheet" href="./Libraries/jQuery/jquery-ui.css">
-        <link rel="stylesheet" href="./Libraries/jQuery/jquery-ui.theme.css">        
+        <!-- jQuery UI styles -->
+        <link rel="stylesheet" href="./Libraries/jQuery/jquery-ui.css?v=<?php echo time(); ?>">
+        <link rel="stylesheet" href="./Libraries/jQuery/jquery-ui.theme.css?v=<?php echo time(); ?>">  
+        
+        <!-- jQuery Select2 styles -->
+        <link rel="stylesheet" href="./Libraries/jQuery/select2.min.css?v=<?php echo time(); ?>">
+
+        <!-- jQuery -->
+        <script src="./Libraries/jQuery/jquery-3.5.1.js?v=<?php echo time(); ?>"></script>
+
+        <!-- jQuery UI -->
+        <script src="./Libraries/jQuery/jquery-ui.js?v=<?php echo time(); ?>"></script>     
+
+        <!-- jQuery Select2 -->
+        <script src="./Libraries/jQuery/select2.full.min.js?v=<?php echo time(); ?>""></script>
     </head>
 
     <body>
-        <!-- jQuery -->
-        <script src="./Libraries/jQuery/jquery-3.5.1.js"></script>
-
-        <!-- jQuery UI -->
-        <script src="./Libraries/jQuery/jquery-ui.js"></script>
+        
     </body>
 </html>
