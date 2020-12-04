1483c32fda36237669f48b79c9febc5347581714
K-Kyler <khaiquang690@gmail.com>
2020-12-04 12:16:24 +0700

    Fix management responsive

--- management.php
+++ management.php
@@ -3,77 +3,88 @@
     require_once('./Config/config.php');
 ?>
 
-<style>
-    <?php 
-        require_once('./CSS/management.css');
-    ?>
-</style>
-
 <?php
-    // Layout
-    require_once('layout.php');
+    if (isset($_SESSION['user_type'])) {
+        if ($_SESSION['user_type'] == 'admin' || $_SESSION['user_type'] == 'staff') {
+            // Layout
+            require_once('layout.php');
 
-    ?>
-        <!-- Page background -->
-        <div class="management-background"></div>
+            ?>
+                <style>
+                    <?php
+                        // Management styles
+                        require_once('./CSS/management.css');   
+                    ?>
+                </style>
+            <?php
 
-        <!-- Management header -->
-        <div class="management-header">
-            <div class="company-logo">
-                <img src="./Images/football.svg" alt="">
-                <div class="company-name">KHV Ground</div>
-            </div>
+            ?>
+                <!-- Page background -->
+                <div class="management-background"></div>
 
-            <div class="header-control">
-                <div class="management-title">
-                    <?= $_SESSION['user_type'] ?> Dashboard
-                    <i class="fas fa-chevron-down"></i>
-                </div>
+                <!-- Management header -->
+                <div class="management-header">
+                    <div class="company-logo">
+                        <img src="./Images/football.svg" alt="">
+                        <div class="company-name">KHV Ground</div>
+                    </div>
 
-                <div class="management-options">
-                    <a href="management.php?m=bookingground_payment">Quản lý đặt sân và thanh toán</a>
-                    <a href="management.php?m=usermanagement">Quản lý người dùng</a>
-                    <a href="management.php?m=beveragemanagement">Quản lý đồ uống</a>
-                    <a href="management.php?m=statisticprofit">Thống kê doanh thu</a>
-                    <a href="index.php">Về trang chủ</a>
-                    <a href="logout.php">Đăng xuất</a>
-                </div>
-            </div>
-        </div>
+                    <div class="header-control">
+                        <div class="management-title">
+                            <?= $_SESSION['user_type'] ?> Dashboard
+                            <i class="fas fa-chevron-down"></i>
+                        </div>
 
-        <!-- Management container -->
-        <div class="management-container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="management">
-                        <?php
-                            if (isset($_GET['m'])) {
-                                if ($_GET['m'] == "bookingground_payment") {
-                                    require_once('./booking-payment.php');
-                                }
-
-                                else if ($_GET['m'] == "usermanagement") {
-                                    require_once('./user-management.php');
-                                }
-
-                                else if ($_GET['m'] == "beveragemanagement") {
-                                    require_once('./beverage-management.php');
-                                }
-
-                                else if ($_GET['m'] == "statisticprofit") {
-
-                                }
-                            }
-    
-                            else {
-                                ?>
-                                    <div class="empty-function-message">Bạn hãy chọn chức năng để hiển thị...</div>
-                                <?php
-                            }
-                        ?>
+                        <div class="management-options">
+                            <a href="management.php?m=bookingground_payment">Quản lý đặt sân và thanh toán</a>
+                            <a href="management.php?m=usermanagement">Quản lý người dùng</a>
+                            <a href="management.php?m=beveragemanagement">Quản lý đồ uống</a>
+                            <a href="management.php?m=statisticprofit">Thống kê doanh thu</a>
+                            <a href="index.php">Về trang chủ</a>
+                            <a href="logout.php">Đăng xuất</a>
+                        </div>
                     </div>
                 </div>
-            </div>            
-        </div>
-    <?php
+
+                <!-- Management container -->
+                <div class="management-container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="management">
+                                <?php
+                                    if (isset($_GET['m'])) {
+                                        if ($_GET['m'] == "bookingground_payment") {
+                                            require_once('./booking-payment.php');
+                                        }
+
+                                        else if ($_GET['m'] == "usermanagement") {
+                                            require_once('./user-management.php');
+                                        }
+
+                                        else if ($_GET['m'] == "beveragemanagement") {
+                                            require_once('./beverage-management.php');
+                                        }
+
+                                        else if ($_GET['m'] == "statisticprofit") {
+
+                                        }
+                                    }
+            
+                                    else {
+                                        ?>
+                                            <div class="empty-function-message">Bạn hãy chọn chức năng để hiển thị...</div>
+                                        <?php
+                                    }
+                                ?>
+                            </div>
+                        </div>
+                    </div>            
+                </div>
+            <?php
+        }
+    }
+
+    else {
+        header("Location: login.php");
+    }
 ?>
